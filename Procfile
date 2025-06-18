224175a03e9e1885f7cd8902d90210fba2143266
Hakimi Fatima-zahra <hakimifatimazahra0@gmail.com>
2025-06-18 17:55:25 +0100

    Déploiement initial sur Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn otmaproject.wsgi
--- dicussion_otma/settings.py
+++ dicussion_otma/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t!xpd8t#1(=-bqqv&n@4%%qvjeti12&$_2ire+)d3)krjo$^bm
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+DEBUG = False  # très important pour la production
 
 
 # Application definition
@@ -140,5 +140,16 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'  # où renvoyer après déconnexion
 
 # settings.py
 LOGIN_REDIRECT_URL = '/redirect_user/'
+import django_heroku
+import dj_database_url
+import os
+from pathlib import Path
+
+# Pour servir les fichiers statiques
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Active la config Heroku
+django_heroku.settings(locals())
 
 
